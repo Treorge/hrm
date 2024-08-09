1846854600dcc5066afe7af79c7385cb743b8d1c
huangcj <1515592489@qq.com>
2024-08-09 17:35:13 +0800

    新增角色功能

--- src/views/role/index.vue
+++ src/views/role/index.vue
@@ -2,7 +2,7 @@
   <div class="container">
     <div class="app-container">
       <div class="role-operate">
-        <el-button type="primary" size="mini">添加角色</el-button>
+        <el-button type="primary" size="mini" @click="addRole">添加角色</el-button>
       </div>
       <el-table :data="roleList">
         <el-table-column prop="name" align="center" width="200" label="角色" />
@@ -20,10 +20,31 @@
         <el-pagination :page-size="pageParams.pagesize" :current-page="pageParams.currentpage" :total="pageParams.total" layout="prev, pager, next" @current-change="pageChange" />
       </el-row>
     </div>
+    <el-dialog title="新增角色" :visible.sync="showDialog" @close="close">
+      <el-form ref="roleForm" :model="roleForm" :rules="rules" label-width="120px">
+        <el-form-item prop="name" label="角色名称">
+          <el-input v-model="roleForm.name" type="text" style="width:300px" size="mini" placeholder="请输入角色名称" />
+        </el-form-item>
+        <el-form-item label="启用">
+          <el-switch v-model="roleForm.state" :active-value="1" :inactive-value="0" size="mini" />
+        </el-form-item>
+        <el-form-item prop="description" label="角色描述">
+          <el-input v-model="roleForm.description" type="textarea" :rows="3" style="width:300px" size="mini" placeholder="请输入角色描述" />
+        </el-form-item>
+        <el-form-item>
+          <el-row type="flex" justify="center">
+            <el-col :span="12">
+              <el-button type="primary" size="mini" @click="handleAdd">确定</el-button>
+              <el-button size="mini" @click="close">取消</el-button>
+            </el-col>
+          </el-row>
+        </el-form-item>
+      </el-form>
+    </el-dialog>
   </div>
 </template>
 <script>
-import { getRoleList } from '@/api/role'
+import { addRole, getRoleList } from '@/api/role'
 
 export default {
   name: 'Role',
@@ -34,6 +55,16 @@ export default {
         pagesize: 10,
         currentpage: 1,
         total: 0
+      },
+      showDialog: false,
+      roleForm: {
+        name: '',
+        state: 0,
+        description: ''
+      },
+      rules: {
+        name: [{ required: true, message: '角色名称不能为空', trigger: 'blur' }],
+        description: [{ required: true, message: '角色描述不能为空', trigger: 'blur' }]
       }
     }
   },
@@ -47,12 +78,34 @@ export default {
       this.roleList = rows
       this.pageParams = total
     },
-    handleCheck() {
-
-    },
     pageChange(newPage) {
       this.pageParams.currentpage = newPage
       this.getRoleList()
+    },
+    handleCheck() {
+
+    },
+    addRole() {
+      this.showDialog = true
+    },
+    handleAdd() {
+      this.$refs.roleForm.validate(async(valid) => {
+        if (valid) {
+          await addRole(this.roleForm)
+          this.$message.success('添加成功')
+          this.getRoleList()
+          this.close()
+        }
+      })
+    },
+    close() {
+      this.showDialog = false
+      this.roleForm = {
+        name: '',
+        state: 0,
+        description: ''
+      }
+      this.$refs.roleForm.resetFields()
     }
   }
 }
